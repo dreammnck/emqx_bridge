31aaab177a61555cfacae8b0f10a72788f3edb3a
saiparnchikidaow <saiparnnnhuhu@gmail.com>
2023-02-17 13:51:48 +0700

    change trigger

--- controller/trigger.py
+++ controller/trigger.py
@@ -1,4 +1,23 @@
 from fastapi import APIRouter
+import pymongo
+from pymongo import MongoClient
+import threading
+
+modelName = []
+#Connect to database
+conn_str = "mongodb+srv://iotHealthcare:edHdnrYXycAmvf4q@dev-iot-healthcare.dk1uvoe.mongodb.net/?retryWrites=true&w=majority"
+try: 
+    client = MongoClient(conn_str)
+    print(client.list_database_names())
+    db = client["iotHealthcare"]
+    collection = db["medicalModel"]
+    results = collection.find({})
+    for result in results:
+        modelName.append(result["modelName"])
+    print(modelName)
+except Exception:
+    print("Error:" + Exception)
+
 
 
 router = APIRouter(
@@ -6,7 +25,19 @@ router = APIRouter(
     tags=["trigger"],
 )
 
-
+#Trigger
 @router.get("/")
 def trigger():
-    return {"Hello": "trigger"}
+    ##stop thred
+    modelName = []
+    results = collection.find({})
+    for result in results:
+        modelName.append(result["modelName"])
+    ## start Thred
+    return {"trigger"}
+
+
+
+
+
+
