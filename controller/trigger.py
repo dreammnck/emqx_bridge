0f55275e07b05476ccca39bd95bcd50e2cf2d64e
saiparnchikidaow <saiparnnnhuhu@gmail.com>
2023-05-04 15:43:34 +0700

    init

--- controller/trigger.py
+++ controller/trigger.py
@@ -1,4 +1,32 @@
 from fastapi import APIRouter
+<<<<<<< Updated upstream
+=======
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+from multiprocessing import Process
+from bridge.mqtt_bridge import send_all_data, modelName
+
+load_dotenv("/Users/marineyatajoparung/Documents/GitHub/emqx_bridge/env/.env")
+
+
+
+process = Process(target=send_all_data)
+process.start()
+
+
+  
+#Connect to database
+try: 
+    client = MongoClient(os.getenv("CONNECTION_STRING"))
+    db = client["iotHealthcare"]
+    collection = db["medicalModel"]
+    results = collection.find({})
+    for result in results:
+       modelName.append(result["modelName"])
+except Exception:
+    print("Error:" + Exception)
+>>>>>>> Stashed changes
 
 
 router = APIRouter(
@@ -9,4 +37,36 @@ router = APIRouter(
 
 @router.get("/")
 def trigger():
+<<<<<<< Updated upstream
     return {"Hello": "trigger"}
+=======
+    
+    global process
+    global modelName
+    
+    #Stop process
+    process.terminate()
+    
+    modelName = []
+    client = MongoClient(os.getenv("CONNECTION_STRING"))
+    db = client["iotHealthcare"]
+    collection = db["medicalModel"]
+    results = collection.find({})
+    for result in results:
+        modelName.append(result["modelName"])
+    print(modelName)
+    
+
+    #Start new process
+    process = Process(target=send_all_data)
+    process.start()
+
+
+    return {"triggerd"}
+
+
+
+
+
+
+>>>>>>> Stashed changes
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+<<<<<<< Updated upstream
 from controller import trigger
+=======
+from controller.trigger import router
+from bridge.mqtt_bridge import send_all_data
+
+>>>>>>> Stashed changes
 
 app = FastAPI()
 app.include_router(trigger.router)
 
+<<<<<<< Updated upstream
+=======
+@app.get("/")
+def root():
+    return {"started"}
+    
+    
+    
+>>>>>>> Stashed changes
 
 if __name__ == "__main__":
     @app.get("/")
